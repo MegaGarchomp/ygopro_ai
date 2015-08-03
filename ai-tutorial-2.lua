077a0ccbfb91355216e53f1d7e3e4443797d7548
MegaGarchomp <teru_vaio@hotmail.co.jp>
2015-08-04 00:53:26 +0900

    8/4

--- ai-tutorial-2.lua
+++ ai-tutorial-2.lua
@@ -98,69 +98,83 @@ function OnSelectSum(cards, sum, triggeringCard)
 end
 
 function OnSelectCard(cards, minTargets, maxTargets, triggeringID, triggeringCard)
-  local result = {}
-  if Duel.GetCurrentPhase()==PHASE_BATTLE 
-  and GlobalAIIsAttacking 
-  and Duel.GetCurrentChain()==0
-  and not triggeringCard
-  then 
-	  AI.Chat("SelectCardEvent")
-	  local tblAI = {}
-	  local tblOpp = {}
-	  
-	  local AIMon = AI.GetAIMonsterZones()
-	  
-	  local AIOppMon = AI.GetOppMonsterZones()
-
-	  for i=1,#AIMon do
-		  if AIMon[i] ~= false then
-			  tblAI[i] = AIMon[i]
-			  tblAI[i].position = i
-		  else
-			  AI.Chat("No card")
-		  end
-	  end
+  local result = {1}
+  AI.Chat("a")
+  if Duel.GetCurrentPhase() == PHASE_BATTLE then
+	AI.Chat("b")
+	local tblAI = {}
+	local tblOpp = {}
   
-	  table.sort(tblAI,
-		  function(a,b)
-			 return (a.attack < b.attack)
-		  end
-		  )
-		  
-		  
-		for i=1,#AIOppMon do
-			if AIOppMon[i] ~= false then
-				tblOpp[i] = AIOppMon[i]
-				tblOpp[i].position = i
-			else 
-				AI.Chat("No card")
-			end
+	local AIMon = AI.GetAIMonsterZones()
+  
+	local AIOppMon = AI.GetOppMonsterZones()
+  
+	local tblcount = 1
+	for i=1,#AIMon do
+		if AIMon[i] ~= false then
+			tblAI[tblcount] = AIMon[i]
+			tblAI[tblcount].position = i
+			tblcount = tblcount + 1
+		else
+			print("No card aimon")
 		end
-		
-		table.sort(tblOpp,
-			function(a,b)
-				return (a.attack < b.attack)
-			end
-			)
-		
-		local a = 1
-		local b = #tblAI
-		while b　>= 1 do
-			local i = 1
-			while tblAI[b] > tblOpp[i] do
-				i = i + 1
-			end
-			b = b - 1
-			result[a] = tblOpp[i].position
-			a = a + 1
+	end
+  
+	table.sort(tblAI,
+		function(a,b)
+			return (a.attack < b.attack)
 		end
-		
-	  
+		)
+	for i=1,#tblAI do
+		AI.Chat(tblAI[i].position)
+		print(tblAI[i].attack)
+	end
+  
+	local oppmoncount = 1
+	for i=1, #AIOppMon do  -- tblOppにアクセスするとエラーが出る
+		tblOpp[oppmoncount] = AIOppMon[i]
+		tblOpp[oppmoncount].position = oppmoncount
+		AI.Chat(tblOpp[oppmoncount].position)
+		print("oppmon")
+		oppmoncount = oppmoncount + 1
 	end
 	
-  --[[for i=1,minTargets do
+	table.sort(tblOpp,
+		function(a,b)
+			return (a.attack < b.attack)
+		end
+		)
+	end
+		
+  
+  
+  
+  --[[for i=1,#AIMon do
+                        tblAI[i] = AIMon[i]
+                end
+                table.sort(tblAI,
+                        function(a,b)
+                                return (a.attack < b.attack)
+                        end
+                )
+                for k=1,#AIOppMon do
+                        tblOpp[k] = AIOppMon[k]
+                end
+                table.sort(tblOpp,
+                        function(a,b)
+                                return (a.attack < b.attack)
+                        end
+                )
+        end]]
+--[[        local attackTarget=1
+        for i=1,#tblAI do
+                for k=1,#tblOpp do        
+                        if (tblAI[i].attack > tblOpp[k].attack) then
+                                attackTarget = tblOpp[k] ]]
+        
+  for i=1,minTargets do
     result[i]=i
-  end]]
+  end
   return result
 end
 
@@ -192,9 +206,23 @@ function OnSelectBattleCommand(cards, activatable_cards)
 		end
 		return highestIndex
 	end
-        AI.Chat("OnSelectBattleCommand")
-        AI.Chat(#cards,#activatable_cards)
-	if #cards > 0 --[[and cards[getHighestAttackerIndex()].attack > getWeakestAttack()]] then
+	
+	local function getAIWeakestAttackerIndex()
+		local lowestIndex = 1
+		local lowestAttack = cards[1].attack
+		for i=2,#cards do
+			if cards[i].attack < lowestAttack then
+				lowestIndex = i
+				lowestAttack = cards[i].attack
+			end
+		end
+		return lowestIndex
+	end
+
+    AI.Chat("OnSelectBattleCommand")
+    AI.Chat(#cards,#activatable_cards)
+	
+	if #cards > 0 and getWeakestAttack() ~= -1 then
 		command = CMD_ATTACK
 		index = getHighestAttackerIndex()
                 monattack = getWeakestAttack()
@@ -202,7 +230,9 @@ function OnSelectBattleCommand(cards, activatable_cards)
                     index = 0
                     command = CMD_STOP
                 end
-                    
+    elseif #cards > 0 and getWeakestAttack() == -1 then --ゴーズケア
+		command = CMD_ATTACK
+		index = getAIWeakestAttackerIndex()
 	elseif #activatable_cards > 0 then
 		command = CMD_ACTIVATE
 		index = 1
